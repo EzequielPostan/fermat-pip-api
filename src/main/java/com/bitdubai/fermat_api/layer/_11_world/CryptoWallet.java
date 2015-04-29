94be8c3e78fc27f708acfdea9f228a8c8e119e24
lnacosta <laion.cj91@gmail.com>
2015-04-29 13:29:35 -0300

    Se agregan metodos basicos a la CryptoWallet y se generan en la billetera Blockchain, se realizo una reorganizacion del codigo. Se tiene en cuenta la posibilidad de multi-address para una wallet.

--- src/main/java/com/bitdubai/fermat_api/layer/_11_world/CryptoWallet.java
+++ src/main/java/com/bitdubai/fermat_api/layer/_11_world/CryptoWallet.java
@@ -1,12 +1,15 @@
 package com.bitdubai.fermat_api.layer._11_world;
 
 import com.bitdubai.fermat_api.layer._11_world.blockchain_info.exceptions.CantGetAddressBalanceException;
+import com.bitdubai.fermat_api.layer._11_world.blockchain_info.exceptions.CantGetAddressesException;
+import com.bitdubai.fermat_api.layer._11_world.blockchain_info.exceptions.CantGetNewAddressException;
 import com.bitdubai.fermat_api.layer._11_world.blockchain_info.exceptions.CantGetWalletBalanceException;
 import com.bitdubai.fermat_api.layer._11_world.blockchain_info.exceptions.CantSendCryptoException;
 import com.bitdubai.fermat_api.layer._11_world.blockchain_info.exceptions.CantStartBlockchainInfoWallet;
 import com.bitdubai.fermat_api.layer._1_definition.enums.CryptoCurrency;
 import com.bitdubai.fermat_api.layer._1_definition.money.CryptoAddress;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -14,16 +17,23 @@ import java.util.UUID;
  */
 public interface CryptoWallet {
 
-    
+
     public void start() throws CantStartBlockchainInfoWallet;
-    
+
     public void stop();
-    
+
     public long getWalletBalance(CryptoCurrency cryptoCurrency) throws CantGetWalletBalanceException;
-    
+
     public long getAddressBalance(CryptoAddress cryptoAddress) throws CantGetAddressBalanceException;
-    
-    public void sendCrypto (CryptoCurrency cryptoCurrency, long amount, CryptoAddress cryptoAddress) throws CantSendCryptoException;
-    
-    
+
+    // to get all the addresses in a wallet
+    public List<CryptoAddress> getAddresses() throws CantGetAddressesException;
+
+    // to create new address in a wallet
+    public CryptoAddress getNewAddress() throws CantGetNewAddressException;
+
+    // added CryptoAddressFrom to choose the address where i sending from in my wallet
+    // can be null, and the method haves to use the first or primary address in wallet
+    public void sendCrypto (CryptoAddress cryptoAddressFrom, CryptoCurrency cryptoCurrency, long amount, CryptoAddress cryptoAddressTo) throws CantSendCryptoException;
+
 }
---
+++ src/main/java/com/bitdubai/fermat_api/layer/_11_world/blockchain_info/exceptions/CantGetAddressesException.java
@@ -0,0 +1,8 @@
+package com.bitdubai.fermat_api.layer._11_world.blockchain_info.exceptions;
+
+/**
+ * Created by leon on 4/28/15.
+ */
+public class CantGetAddressesException extends  Exception {
+}
+
