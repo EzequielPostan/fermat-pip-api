871585ea9d2f874e9d8a97429a816ba14450775e
Jorge Gonzalez <jorgeejgonzalez@gmail.com>
2015-05-19 11:59:13 -0430

    added the interface for the Cloud FMP Connections

---
+++ src/main/java/com/bitdubai/fermat_api/layer/_10_communication/cloud/CloudFMPConnectionManager.java
@@ -0,0 +1,10 @@
+package com.bitdubai.fermat_api.layer._10_communication.cloud;
+
+import com.bitdubai.fermat_api.layer._10_communication.fmp.FMPPacketHandler;
+
+/*
+ *	created by jorgeejgonzalez
+ */
+public interface CloudFMPConnectionManager extends CloudConnectionManager, FMPPacketHandler {
+	
+}
