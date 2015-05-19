13a5d2d4cb86090c6816b0d765fd52913d1b8801
Jorge Gonzalez <jorgeejgonzalez@gmail.com>
2015-05-19 12:07:51 -0430

    renamed the connection agent

---
+++ src/main/java/com/bitdubai/fermat_api/layer/_10_communication/cloud/CloudConnectionAgent.java
@@ -0,0 +1,9 @@
+package com.bitdubai.fermat_api.layer._10_communication.cloud;
+
+/*
+ *	Created by jorgeejgonzalez
+ */
+public interface CloudConnectionAgent {
+	public void start() throws CloudConnectionException;
+	public void stop() throws CloudConnectionException;
+}
--- src/main/java/com/bitdubai/fermat_api/layer/_10_communication/cloud/ConnectionAgent.java
+++
@@ -1,9 +0,0 @@
-package com.bitdubai.fermat_api.layer._10_communication.cloud;
-
-/*
- *	Created by jorgeejgonzalez
- */
-public interface ConnectionAgent {
-	public void start();
-	public void stop();
-}
