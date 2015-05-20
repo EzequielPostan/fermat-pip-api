02a2c9eba8b3c996f84ef4a2584d46562b1e86af
Jorge Gonzalez <jorgeejgonzalez@gmail.com>
2015-05-19 23:16:36 -0430

    added the getSocketAddress method to the CloudConnectionAddress interface

--- src/main/java/com/bitdubai/fermat_api/layer/_10_communication/cloud/CloudConnectionAddress.java
+++ src/main/java/com/bitdubai/fermat_api/layer/_10_communication/cloud/CloudConnectionAddress.java
@@ -1,10 +1,14 @@
 package com.bitdubai.fermat_api.layer._10_communication.cloud;
 
+import java.net.InetSocketAddress;
+
 import com.bitdubai.fermat_api.layer._10_communication.CommunicationChannelAddress;
 
 /**
  * Created by jorgeejgonzalez
  */
 public interface CloudConnectionAddress extends CommunicationChannelAddress {
+
+	public InetSocketAddress getSocketAddress();	
 
 }
