6ddbc7976aa2695cdf2083759f788f5ce2b4d302
rodrigoA <acosta_rodrigo@hotmail.com>
2015-05-31 12:29:28 -0300

    fixed wrong exception reference.

--- src/main/java/com/bitdubai/fermat_api/layer/_7_crypto_network/CryptoNetworkSubsystem.java
+++ src/main/java/com/bitdubai/fermat_api/layer/_7_crypto_network/CryptoNetworkSubsystem.java
@@ -6,6 +6,6 @@ import com.bitdubai.fermat_api.Plugin;
  * Created by ciencias on 30.12.14.
  */
 public interface CryptoNetworkSubsystem {
-    public void start () throws com.bitdubai.fermat_api.layer._7_crypto_network.bitcoin.exceptions.CantStartSubsystemException;
+    public void start () throws com.bitdubai.fermat_api.layer._7_crypto_network.CantStartSubsystemException;
     public Plugin getPlugin();
 }
