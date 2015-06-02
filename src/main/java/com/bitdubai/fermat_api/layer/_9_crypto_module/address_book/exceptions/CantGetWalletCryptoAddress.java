67c144d6b04220af6a166e20478da0880f5f84c2
Luis Fernando Molina <luis.molina@bitDubai.com>
2015-06-02 17:07:41 +0200

    2 Exceptions added to API by Armando

---
+++ src/main/java/com/bitdubai/fermat_api/layer/_9_crypto_module/address_book/exceptions/CantGetWalletCryptoAddress.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_api.layer._9_crypto_module.address_book.exceptions;
+
+/**
+ * Created by armando on 29/05/15.
+ */
+public class CantGetWalletCryptoAddress extends Exception {
+}
