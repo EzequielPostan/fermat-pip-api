83ea76523b6264eefe212c58ab8161a5a6d17fd3
León <laion.cj91@gmail.com>
2015-06-10 13:07:06 -0300

    Updated interfaces for plugin Wallet Contacts
    Changed name from WalletContacsManager to WalletContactsManager.
    Added methods:
    - listWalletContacts
    - listWalletContactsScrolling
    - createWalletContact
    - updateWalletContact
    - deleteWalletContact

---
+++ src/main/java/com/bitdubai/fermat_api/layer/_15_middleware/wallet_contacts/WalletContactsManager.java
@@ -0,0 +1,34 @@
+package com.bitdubai.fermat_api.layer._15_middleware.wallet_contacts;
+
+import com.bitdubai.fermat_api.layer._15_middleware.wallet_contacts.exceptions.CantCreateWalletContactException;
+import com.bitdubai.fermat_api.layer._15_middleware.wallet_contacts.exceptions.CantDeleteWalletContactException;
+import com.bitdubai.fermat_api.layer._15_middleware.wallet_contacts.exceptions.CantGetAllWalletContactsException;
+import com.bitdubai.fermat_api.layer._15_middleware.wallet_contacts.exceptions.CantUpdateWalletContactException;
+import com.bitdubai.fermat_api.layer._1_definition.money.CryptoAddress;
+import com.bitdubai.fermat_api.layer._5_user.UserTypes;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * The Class <code>com.bitdubai.fermat_api.layer._15_middleware.wallet_contacts.WalletContactsManager</code>
+ * indicates the functionality of a WalletContactsManager
+ * <p/>
+ *
+ * Created by loui on 18/02/15.
+ * Reviewed by Leon Acosta - (laion.cj91@gmail.com) on 08/06/15.
+ * @version 1.0
+ * @since Java JDK 1.7
+ */
+public interface WalletContactsManager {
+
+    public List<WalletContact> listWalletContacts(UUID walletId) throws CantGetAllWalletContactsException;
+
+    public List<WalletContact> listWalletContactsScrolling(UUID walletId, Integer max, Integer offset) throws CantGetAllWalletContactsException;
+
+    public WalletContact createWalletContact(CryptoAddress cryptoAddress, UUID userId, String userName, UserTypes userType, UUID walletId) throws CantCreateWalletContactException;
+
+    public void updateWalletContact(WalletContact walletContact) throws CantUpdateWalletContactException;
+
+    public void deleteWalletContact(UUID contactId) throws CantDeleteWalletContactException;
+}
